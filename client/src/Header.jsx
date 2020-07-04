2a956ff7d6e525aaa2d9b23351639a1d4a0d830d
Stephen <code@cptnfizzbin.ca>
2020-07-04 16:11:19 -0400

    import header into react client

---
+++ client/src/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './Header.scss';
+
+const Header = () => (
+    <header>
+        <div className="surface">
+            <div className="names">Doug &amp; Mabel Mercer</div>
+            <img className="image" src="/img/header.jpg" alt="Doug &amp; Mabel Mercer"/>
+
+            <div className="doug">
+                <div className="name">Douglas William Mercer</div>
+                <div className="dates">1939 - 2020</div>
+            </div>
+
+            <div className="mabel">
+                <div className="name">Mabel Armorel Mercer</div>
+                <div className="dates">1938 - 2019</div>
+            </div>
+        </div>
+    </header>
+)
+
+export default Header;

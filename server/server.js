3e08c3ebdaf8d69e36ea871a6288851fbb6912db
Stephen <code@cptnfizzbin.ca>
2020-07-04 23:48:17 -0400

    add images to comments

--- server/server.js
+++ server/server.js
@@ -1,28 +1,37 @@
 import express from 'express';
 import {readdir, readFile} from 'fs';
 import {promisify} from "util";
-import {dirname, join} from "path";
+import path, {dirname} from "path";
 import {fileURLToPath} from "url";
 import cors from 'cors';
 
 const readDirPromise = promisify(readdir);
 const readFilePromise = promisify(readFile);
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const dataDir = path.join(__dirname, './data/comments');
 
 const app = express();
 app.use(cors());
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/img/:file', (req, res) => {
+    res.sendFile(path.join(dataDir, req.params.file));
+});
+
 app.get('/comments', async (req, res, next) => {
     try {
-        const dataDir = join(__dirname, './data/comments');
 
         let files = await readDirPromise(dataDir);
         files = files.filter((file) => file.match('\.json$'));
 
         const comments = [];
         for(const file of files) {
-            const commentJson = await readFilePromise(join(dataDir, file));
-            comments.push(JSON.parse(commentJson));
+            const commentJson = await readFilePromise(path.join(dataDir, file));
+            const comment = JSON.parse(commentJson)
+            comment.id = path.parse(file).name
+            comments.push(comment);
         }
         res.send({comments});
     } catch (error) {

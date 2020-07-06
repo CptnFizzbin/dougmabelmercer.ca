c294ee3e489899d38ede6511bacbb35454f19f0c
Stephen <code@cptnfizzbin.ca>
2020-07-05 23:38:04 -0400

    add accessibility keys

--- client/src/comments/AddComment.jsx
+++ client/src/comments/AddComment.jsx
@@ -21,6 +21,7 @@ const AddComment = () => {
 
     const onAuthorChange = (event) => setAuthor(event.target.value);
     const onContentChange = (event) => setContent(event.target.value);
+    const onKeyDown = (event) => event.key === "Enter" ? onSave() : null;
     const onSave = async () => {
         if (saving) return;
 
@@ -46,12 +47,12 @@ const AddComment = () => {
                     <textarea placeholder={"Message..."} onChange={onContentChange} disabled={saving} value={content}/>
                     {errors.content ? <div className={'error'}>{errors.content}</div> : null}
                 </div>
-                <div className="text">
+                <div className="text" onKeyDown={onKeyDown}>
                     <input placeholder={'Name'} onChange={onAuthorChange} value={author} disabled={saving}/>
                     {errors.author ? <div className={'error'}>{errors.author}</div> : null}
                 </div>
                 <div className="text">
-                    <div className="save-btn" onClick={onSave}>{saving ? "Saving..." : "Add Memory"}</div>
+                    <div className="save-btn" onClick={onSave} tabIndex={0} onKeyDown={onKeyDown}>{saving ? "Saving..." : "Add Memory"}</div>
                 </div>
             </div>
         </div>
